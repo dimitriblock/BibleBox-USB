7ac059e017ae772c8cf44eefd2abdacafdbc6bb4
Dimitri Block <dimitriblock@gmail.com>
2015-11-21 14:23:13 +0100

    Extract h5ai; adapt install script

--- install_h5ai.sh
+++ install_h5ai.sh
@@ -1,11 +1,9 @@
-	#!/bin/sh
+#!/bin/sh
 #
 clear
 echo "-------------------"
 echo "_h5ai Installer"
 echo "-------------------"
-echo "Unzipping Install Folder"
-tar -xvf h5ai_installer.tar
 cd h5ai_installer/
 echo "Installing h5ai"
 cp -fr _h5ai /opt/piratebox/www/
@@ -13,9 +11,6 @@ echo "Modifying files"
 cp -f mime.types /opt/piratebox/conf/lighttpd/
 cp -f lighttpd.conf /opt/piratebox/conf/lighttpd/
 echo "Select 'A' if prompted to overwrite"
-echo "Installation complete - cleaning up temp files"
-cd ..
-rm -rf h5ai_installer
 echo "--------------------------"
 echo "= Finished h5ai install  ="
 echo "--------------------------"
